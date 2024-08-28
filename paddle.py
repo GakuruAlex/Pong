7c762793f120d0c49d2708ccec3dc7beb897140a
alexgicimo <gakurualexgicimo@gmail.com>
2024-08-28 10:45:49 +0000

    create paddles for the players

--- paddle.py
+++ paddle.py
@@ -1,14 +1,36 @@
 from turtle import Turtle
 STARTING_POSITION ={
-    "player_one": [(0,0), (0, 20), (0, -20)],
-    "player_two": 
+    "player_one": [(-390,-20), (-390, 0), (-390, 20)],
+    "player_two": [(390, -20), (390, 0), (390, 20)]
 }
-class Paddles(Turtle):
+class Paddles:
     def __init__(self):
-        super.__init__()
-        self.paddles = []
+        self.paddles = {"player_one":[], "player_two": []}
         self.set_paddle()
-    
-    def add_turtle(self, position)
+        self.player_one_head = self.paddles["player_one"][0]
+        self.player_two_head = self.paddles["player_two"][0]
+        self.move_paddle("player_one")
+
+
+
+    def add_turtle(self,player, position):
+        for coord in position:
+            turtle = Turtle()
+            turtle.shape("square")
+            turtle.color("white")
+            turtle.speed("fastest")
+            turtle.penup()
+            x_cor, y_cor = coord
+            turtle.goto(x=x_cor, y=y_cor)
+            turtle.setheading(90)
+            self.paddles[player].append(turtle)
+
+
     def set_paddle(self):
-        pass
+        for player,position in STARTING_POSITION.items():
+            self.add_turtle(player, position)
+
+    def move_paddle(self, player):
+        for paddle in self.paddles[player]:
+            paddle.forward(20)
+
