642546f0f329b3dd93382a8577cbc100db00979d
alexgicimo <gakurualexgicimo@gmail.com>
2024-08-28 12:14:35 +0000

    player one moving left

--- paddle.py
+++ paddle.py
@@ -7,9 +7,7 @@ class Paddles:
     def __init__(self):
         self.paddles = {"player_one":[], "player_two": []}
         self.set_paddle()
-        self.player_one_head = self.paddles["player_one"][0]
-        self.player_two_head = self.paddles["player_two"][0]
-        self.move_paddle("player_one")
+
 
 
 
@@ -30,4 +28,14 @@ class Paddles:
         for player,position in STARTING_POSITION.items():
             self.add_turtle(player, position)
 
-   
+    def one_move_left(self):
+
+        for tur in self.paddles["player_one"]:
+            tur.setheading(90)
+            x_cor = tur.xcor()
+            y_cor = tur.ycor() + 20
+            tur.goto(x=x_cor, y=y_cor)
+    def move_one_right(self):
+        
+
+
