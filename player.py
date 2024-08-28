9ec9b312c4af49761d2ddbd1f16dce59cbf2d437
alexgicimo <gakurualexgicimo@gmail.com>
2024-08-28 10:55:28 +0000

    move paddle

--- player.py
+++ player.py
@@ -7,6 +7,17 @@ class Player(Turtle):
         self.player_score = 0
         self.penup()
         self.hideturtle()
-        
+    paddle = Paddles()
+    
     def increase_score(self):
         self.player_score += 1
+   
+    def move(self, player):
+        for paddle in self.paddle.paddles[player]:
+            paddle.forward(20)
+    def move_right(self, player):
+        self.setheading(90)
+        self.move(player)
+    def move_left(self, player):
+        self.setheading(270)
+        self.move(player)
