7a5ad62d8cf7c86baa11ced14f3fd04d3d6b266e
alexgicimo <gakurualexgicimo@gmail.com>
2024-08-28 07:08:42 +0000

    pong game

---
+++ main.py
@@ -0,0 +1,8 @@
+from turtle import Screen,Turtle
+
+def main()-> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ screen.py
@@ -0,0 +1,4 @@
+from turtle import Turtle
+class Display:
+    def display()-> None:
+        
