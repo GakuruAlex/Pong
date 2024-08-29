d0c654d052b75cad99df948cfa7a7a84585093a5
alexgicimo <gakurualexgicimo@gmail.com>
2024-08-29 12:28:00 +0000

    ball mechanics

--- ball.py
+++ ball.py
@@ -9,7 +9,6 @@ class Ball(Turtle):
 
 
     def move(self):
-        
-        x_cor = self.xcor() + 5
-        y_cor = self.ycor() + 5
+        x_cor = self.xcor() + 10
+        y_cor = self.ycor() + 10
         self.goto(x=x_cor, y=y_cor)
