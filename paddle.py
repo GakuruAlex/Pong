cbc24fd44f77d52c84f977ab1fdc371b4e7931f9
alexgicimo <gakurualexgicimo@gmail.com>
2024-08-29 11:30:50 +0000

    move paddle up and down

--- paddle.py
+++ paddle.py
@@ -5,11 +5,17 @@ class Paddle(Turtle):
         super().__init__()
         self.shape("square")
         self.color("white")
-        self.shapesize(stretch_len=1, stretch_wid=5)
+        self.shapesize(stretch_len=1, stretch_wid=4)
         self.speed("fastest")
         self.penup()
         self.goto(position)
-
-
+    def up(self):
+        y_cor =self.ycor()
+        x_cor = self.xcor() + 20
+        self.goto(x=x_cor, y=y_cor)
+    def down(self):
+        x_cor = self.xcor()
+        y_cor=self.ycor() -20
+        self.goto(x=x_cor, y=y_cor)
 
    
