b82e1c9a1f42d287763737a68311bd766be50211
alexgicimo <gakurualexgicimo@gmail.com>
2024-08-29 11:44:48 +0000

    use tracer to show end of animation for creating a paddle

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ STARTING_POSITION ={
 }
 
 def main()->None:
+    game_is_on = True
     screen = Screen()
     screen.bgcolor("black")
     screen.setup(width=800, height= 600)
     screen.title("Pong Game")
+    screen.tracer(0)
     
     left_paddle = Paddle(STARTING_POSITION["player_one"])
     right_paddle = Paddle(STARTING_POSITION["player_two"])
@@ -21,11 +23,10 @@ def main()->None:
     screen.onkey(left_paddle.down, "s")
     screen.onkey(right_paddle.up, "Up")
     screen.onkey(right_paddle.down, "Down")
+    
+    while game_is_on:
+        screen.update()
 
-
-
-
-    screen.update()
     screen.exitonclick()
 
 if __name__=="__main__":
