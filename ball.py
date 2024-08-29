6bb90129ddd04e69813b076c8319528baa07cfb0
alexgicimo <gakurualexgicimo@gmail.com>
2024-08-29 12:10:23 +0000

    ball class

--- ball.py
+++ ball.py
@@ -1,2 +1,15 @@
+from turtle import Turtle
 
-    
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.shape("circle")
+        self.penup()
+
+
+    def move(self):
+        
+        x_cor = self.xcor() + 5
+        y_cor = self.ycor() + 5
+        self.goto(x=x_cor, y=y_cor)
