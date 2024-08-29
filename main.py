cf3cda9dba598df96e21143c09aaf0f5cd3ccdb2
alexgicimo <gakurualexgicimo@gmail.com>
2024-08-29 11:42:32 +0000

    move up and down functions

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from screen import Display
+
 from turtle import Screen
-from paddle import Paddles
+from paddle import Paddle
 STARTING_POSITION ={
     "player_one": (-390, 0),
     "player_two": (390, 0)
@@ -11,6 +11,22 @@ def main()->None:
     screen.bgcolor("black")
     screen.setup(width=800, height= 600)
     screen.title("Pong Game")
+    
+    left_paddle = Paddle(STARTING_POSITION["player_one"])
+    right_paddle = Paddle(STARTING_POSITION["player_two"])
+
+    screen.listen()
+
+    screen.onkey(left_paddle.up, "w")
+    screen.onkey(left_paddle.down, "s")
+    screen.onkey(right_paddle.up, "Up")
+    screen.onkey(right_paddle.down, "Down")
 
 
+
+
+    screen.update()
     screen.exitonclick()
+
+if __name__=="__main__":
+    main()
--- paddle.py
+++ paddle.py
@@ -5,13 +5,14 @@ class Paddle(Turtle):
         super().__init__()
         self.shape("square")
         self.color("white")
-        self.shapesize(stretch_len=1, stretch_wid=4)
+        self.shapesize(stretch_len=4, stretch_wid=1)
+        self.setheading(90)
         self.speed("fastest")
         self.penup()
         self.goto(position)
     def up(self):
-        y_cor =self.ycor()
-        x_cor = self.xcor() + 20
+        y_cor =self.ycor() + 20
+        x_cor = self.xcor()
         self.goto(x=x_cor, y=y_cor)
     def down(self):
         x_cor = self.xcor()
