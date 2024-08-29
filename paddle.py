bc248ee0bd6ba126bdef4788302aef7ed6ef0a1f
alexgicimo <gakurualexgicimo@gmail.com>
2024-08-29 09:45:58 +0000

    change paddle to just one turtle and use shape size to stretch it

--- paddle.py
+++ paddle.py
@@ -4,32 +4,31 @@ class Paddles:
     def __init__(self):
         self.paddles =[]
 
-    def add_turtle(self, position):
-        for coord in position:
-            turtle = Turtle()
-            turtle.shape("square")
-            turtle.color("white")
-            turtle.speed("fastest")
-            turtle.penup()
-            x_cor, y_cor = coord
-            turtle.goto(x=x_cor, y=y_cor)
 
-            self.paddles.append(turtle)
+
+    def add_turtle(self, position):
+            paddle = Turtle()
+            paddle.shape("square")
+            paddle.color("white")
+            paddle.shapesize(stretch_len=1, stretch_wid=5)
+            paddle.speed("fastest")
+            paddle.penup()
+            x_cor, y_cor = position
+            paddle.goto(x=x_cor, y=y_cor)
+
+            self.paddles.append(paddle)
 
 
     def move_up(self):
         head = self.paddles[0]
         if head.ycor() < 243:
-            for i in range(len(self.paddles) - 1, 0, -1):
-                x_cor = self.paddles[i].xcor()
-                y_cor = self.paddles[i].ycor()
-                self.paddles[i].goto(x=x_cor, y= y_cor + 20)
-            head.goto(x =head.xcor(), y= head.ycor() + 20)
+                x_cor = self.paddles[0].xcor()
+                y_cor = self.paddles[0].ycor()
+                head.goto(x =x_cor, y= y_cor + 20)
+
     def move_down(self):
-        head = self.paddles[-1]
+        head = self.paddles[0]
         if head.ycor() > -240:
-            for i in range(0,len(self.paddles) - 1, 1):
-                x_cor = self.paddles[i].xcor()
-                y_cor = self.paddles[i].ycor()
-                self.paddles[i].goto(x=x_cor, y= y_cor - 20)
-            head.goto(x =head.xcor(), y= head.ycor() - 20)
+            x_cor = self.paddles[0].xcor()
+            y_cor = self.paddles[0].ycor()
+            head.goto(x =x_cor, y= y_cor - 20)
