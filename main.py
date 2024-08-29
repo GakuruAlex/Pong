77d13164bf7968641856564f9fae2b688a539455
alexgicimo <gakurualexgicimo@gmail.com>
2024-08-29 13:30:27 +0000

    show player score

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from player import Player
 STARTING_POSITION ={
     "player_one": (-370, 0),
     "player_two": (370, 0)
@@ -14,7 +15,9 @@ def main()->None:
     screen.bgcolor("black")
     screen.setup(width=800, height= 600)
     screen.title("Pong Game")
-    screen.tracer(0)
+    player_one = Player((-50,270))
+    player_two = Player((50, 270))
+    
     
     left_paddle = Paddle(STARTING_POSITION["player_one"])
     right_paddle = Paddle(STARTING_POSITION["player_two"])
@@ -26,16 +29,24 @@ def main()->None:
     screen.onkey(left_paddle.down, "s")
     screen.onkey(right_paddle.up, "Up")
     screen.onkey(right_paddle.down, "Down")
-    
+
     while game_is_on:
         time.sleep(0.1)
-        screen.update()
+
         ball.move()
         if ball.ycor() > 280 or ball.ycor() < -280:
             ball.bounce_y()
-        if ball.distance(right_paddle) < 50 and ball.xcor() > 340 or ball.distance(left_paddle) < 50 and ball.xcor() < -340:
+        if ball.distance(right_paddle) < 40 and ball.xcor() > 350 or ball.distance(left_paddle) < 40 and ball.xcor() < -350:
             ball.bounce_x()
-        
+        if ball.xcor() > 370:
+            player_one.increase_r_score()
+            ball.reset_position()
+        if ball.xcor() < -370:
+            player_two.increase_l_score()
+            ball.reset_position()
+
+
+
     screen.exitonclick()
 
 if __name__=="__main__":
--- player.py
+++ player.py
@@ -1,14 +1,26 @@
 from turtle import Turtle, Screen
-from paddle import Paddles
+
 
 class Player(Turtle):
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()
         self.color("white")
-        self.player_score = 0
+        self.score = 0
         self.penup()
         self.hideturtle()
-
-    def increase_score(self):
-        self.player_score += 1
-
+        self.goto(position)
+        self.l_score = 0
+        self.r_score = 0
+        self.update_scoreboard()
+        
+    def update_scoreboard(self):
+        self.goto(-100, 200)
+        self.write(self.l_score, align="center", font=("Arial", 24, "normal"))
+        self.goto(100, 200)
+        self.write(self.r_score, align="center", font=("Arial",24, "normal"))
+        
+    def increase_l_score(self):
+        self.l_score += 1
+    def increase_r_score(self):
+        self.r_score += 1
+    
